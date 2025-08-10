aa2a0cda4c31810f12e84b2cd5049df1a1c32630
Nik Tysiachnyi <nikita.tysiachnyi@gmail.com>
2025-08-10 10:09:12 +0200

    Added icon

---
+++ app/icon.tsx
@@ -0,0 +1,24 @@
+import { ImageResponse } from "next/og";
+
+export const size = { width: 32, height: 32 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: "100%",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: 54,
+        }}
+      >
+        🧑‍💻
+      </div>
+    ),
+    size,
+  );
+}
